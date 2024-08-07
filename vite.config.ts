5d5f131bf1f169cc70a1e73e7d06d12e076059d1
hayyaun <hayyanhami@outlook.com>
2024-08-07 21:44:34 +0330

    add auto manifest finder

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,12 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    svgr(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: false,
+    }),
+  ],
 });
