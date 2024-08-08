fac59a905fbdcb057bd9797ebad6ca4869e75058
hayyaun <hayyanhami@outlook.com>
2024-08-08 18:18:47 +0330

    hehe

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     <Routes>
       <Route
         path="/intro"
-        element={history.length ? <Navigate to="/history" replace /> : <PageLayout children={<IntroPage />} />}
+        element={history.length > 3 ? <Navigate to="/history" replace /> : <PageLayout children={<IntroPage />} />}
       />
       <Route path="/list/:pageIndex" element={<PageLayout children={<SymptomsPage />} />} />
       <Route path="/history" element={<PageLayout children={<HistoryPage />} />} />
