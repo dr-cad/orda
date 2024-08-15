91f38261358645212c0ddff06187c5966b7ac5c6
hayyaun <hayyanhami@outlook.com>
2024-08-16 01:41:59 +0330

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    svgr(),
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: { enabled: true },
-      workbox: { globPatterns: ["**/*"] },
-      includeAssets: ["**/*"],
-      manifest: false,
-    }),
-    sentryVitePlugin({
-      org: "dr-cad",
-      project: "orda",
-    }),
-  ],
-  build: {
-    sourcemap: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      svgr(),
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: { enabled: true },
+        workbox: { globPatterns: ["**/*"] },
+        includeAssets: ["**/*"],
+        manifest: false,
+      }),
+      sentryVitePlugin({
+        org: "dr-cad",
+        project: "orda",
+        disable: !!env.VITE_LOCAL_APP,
+      }),
+    ],
+    build: {
+      sourcemap: true,
+    },
+  };
 });
