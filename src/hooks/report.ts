77323d4fc77f8a6d61d930c3e60914daece48576
hayyaun <hayyanhami@outlook.com>
2024-08-16 17:43:02 +0330

    .

--- src/hooks/report.ts
+++ src/hooks/report.ts
@@ -13,7 +13,7 @@ const pickFromRange = (ranges: IReport["ranges"], v: IRange) => {
   let to = "";
 
   for (const range of ranges!) {
-    if (!from && v.a < range.b) from = range.text;
+    if (!from && v.a < range.b) from = range.text; // yes it's correct
     if (!to && v.b <= range.b) to = range.text;
   }
 
