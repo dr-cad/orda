3071be60e8dc1024baf5f0f500d5572dd0129eeb
hayyaun <hayyanhami@outlook.com>
2024-11-15 22:58:58 +0330

    .

--- src/data/ai.ts
+++ src/data/ai.ts
@@ -4,11 +4,11 @@ export const forms: IAiForm[] = [
   {
     content: `<span>Hi, I would be happy if you give me a bit of info about the patient.</span>
     
-    1. What's their name?  
-    2. How old are they?  
-    3. What's their name?  
-    4. What's their gender?  
-    5. What's their name?`,
+1. What's their name?  
+2. How old are they?  
+3. What's their name?  
+4. What's their gender?  
+5. What's their name?`,
     placeholder: "Smith 25 male",
   },
 ];
