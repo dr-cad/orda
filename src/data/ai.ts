4b7976cab3eab50d0201c94b220e9c70d8c8e737
mahdi <mahdi.eagle1999@gmail.com>
2024-11-27 13:40:37 +0330

    ai content changes

--- src/data/ai.ts
+++ src/data/ai.ts
@@ -5,11 +5,10 @@ export const forms: IAiForm[] = [
     content: [
       "<span>Hi, I would be happy if you give me a bit of info about the patient.</span>",
       "",
-      "1. What's their name?",
-      "2. How old are they?",
-      "3. What's their name?",
-      "4. What's their gender?",
-      "5. What's their name?",
+      "1. What's the chief complaint?",
+      "2. what're the clinical signs?",
+      "3. What're clinical examination findings?",
+      "4. What're imaging findings?",
     ].join("\n"),
     placeholder:
       "24 old female with report of swelling or pain which she reported gradually increased in 2years. The lesion was hard on palpation.Panoramic findings illustrates unilateral, solitary, mixed radiolucent and radiopaque non-odontogenic lesion with irregular, ill-defined , and blending border in Rt side of mandible at ramus to incisors region with expansion of cortical bone and loss of lamina dura.",
